1f82414dfdc081f5d507448a1063c7226abb7862
Alexandr Kositskiy <koso-96@mail.ru>
2023-11-22 23:21:09 +0300

    add database.sql

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE table_urls (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255),
+    created_at timestamp
+);
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES table_urls (id),
+    status_code integer,
+    h1 varchar(255),
+    title varchar(255),
+    description text,
+    created_at timestamp
+);
