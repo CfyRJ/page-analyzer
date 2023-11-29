617c422b24b201dc7f6b82faed735568847c7ce3
Alexandr Kositskiy <koso-96@mail.ru>
2023-11-29 21:22:34 +0300

    update database_queries.py

--- page_analyzer/database_queries.py
+++ page_analyzer/database_queries.py
@@ -8,6 +8,13 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
+class Url():
+    def __init__(self, id, name, created_at) -> None:
+        self.id = id
+        self.name = name
+        self.created_at = created_at
+
+
 class TableUrls():
     def insert(url: str) -> bool:
         conn = psycopg2.connect(DATABASE_URL)
@@ -88,8 +95,97 @@ class TableUrls():
         return response
 
 
-class Url():
-    def __init__(self, id, name, created_at) -> None:
-        self.id = id
-        self.name = name
-        self.created_at = created_at
+class UrlChecks:
+    def insert(check_date: dict) -> bool:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+
+        try:
+            cur.execute("""
+                INSERT INTO url_checks (
+                        url_id,
+                        status_code,
+                        h1,
+                        title,
+                        description,
+                        created_at)
+                VALUES (%s, %s, %s, %s, %s, %s);
+                """,
+                        (check_date['url_id'],
+                         check_date['status_code'],
+                         check_date['h1'],
+                         check_date['title'],
+                         check_date['description'],
+                         datetime.datetime.now())
+                        )
+            conn.commit()
+            res = True
+        except psycopg2.Error:
+            res = False
+
+        cur.close()
+        conn.close()
+
+        return res
+
+    @classmethod
+    def select_checks_url(cls, url_id: int) -> list:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+
+        cur.execute("""SELECT * FROM url_checks
+                    WHERE url_id = %s
+                    ORDER BY created_at DESC;""",
+                    (url_id)
+                    )
+        checks = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        checks = [cls.make_check(check) for check in checks]
+
+        return checks
+
+    def make_check(check: list) -> dict:
+        return {'id': check[0],
+                'url_id': check[1],
+                'status_code': check[2],
+                'h1': check[3],
+                'title': check[4],
+                'description': check[5],
+                'data': check[6]}
+
+
+def join_table() -> list:
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    cur.execute("""SELECT id, name FROM table_urls ORDER BY id DESC;""")
+    table_urls = cur.fetchall()
+    cur.execute("""SELECT url_checks.url_id, url_checks.created_at, status_code
+                   FROM url_checks JOIN (
+                       SELECT url_id, MAX(created_at) AS created_at
+                       FROM url_checks GROUP BY url_id) AS tab
+                   ON url_checks.url_id=tab.url_id
+                   AND url_checks.created_at=tab.created_at;""")
+    url_checks = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    res = []
+    for id, name in table_urls:
+        for url_id, created_at, status_code in url_checks:
+            if id == url_id:
+                res.append({'id': id,
+                            'name': name,
+                            'last_data': created_at,
+                            'status_code': status_code})
+                break
+        else:
+            res.append({'id': id,
+                        'name': name,
+                        'last_data': '',
+                        'status_code': ''})
+    return res
